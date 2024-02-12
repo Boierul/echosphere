1831f448bffe6eabd7e3d468da5b06484ff74a98
Boierul <49337867+Boierul@users.noreply.github.com>
2024-02-12 17:12:34 +0100

    Fix: Improved Narvbar styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+import "./globals.css";
+
+import React from "react";
+import { GeistSans } from 'geist/font/sans';
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "./globals.css";
-import {ThemeProvider} from "@/providers/ThemeProvider";
-import React from "react";
 
-const inter = Inter({subsets: ["latin"]});
+import {ThemeProvider} from "@/providers/ThemeProvider";
+
+// const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Echosphere",
@@ -28,7 +31,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>
+        <body className={GeistSans.className}>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
