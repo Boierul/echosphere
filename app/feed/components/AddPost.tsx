c5cb081c7092e2f814c5f574a793135f0888bbcd
Boierul <49337867+Boierul@users.noreply.github.com>
2024-02-18 01:11:03 +0100

    Fix: Finally did it! Solved the AddPost.tsx react-hook-form bs. It took too long :|

--- app/feed/components/AddPost.tsx
+++ app/feed/components/AddPost.tsx
@@ -1,42 +1,35 @@
 "use client"
 
+import Filter from "bad-words";
 import React, {useEffect, useState} from "react";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
 import {useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
 import {Textarea} from "@/components/ui/textarea";
-import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
 import {useToast} from "@/components/ui/use-toast";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {askChatGPTForSuggestion} from "@/requests";
 import Loader from "@/components/Loader";
-import {createPost} from "@/requests/createPost";
-// import {Button, Spacer, Spinner, Textarea} from "@nextui-org/react";
-// import {useSession} from "next-auth/react";
 
-// import Filter from "bad-words";
-// import {createPost} from "@/utils/requestsAPI";
-// import {askChatGPTForSuggestion} from "@/utils/requests/openaiSuggestion";
+import {createPost} from "@/requests/createPost";
+import {askChatGPTForSuggestion} from "@/requests";
 
 // Filter profanities
-// const filter = new Filter();
+const filter = new Filter();
 
 export default function AddPost() {
     // const {data: session} = useSession();
     const router = useRouter();
     const {toast} = useToast()
 
-    // Add a posts content
     const [content, setContent] = useState("");
-    const [loading, setLoading] = useState(false);
-
     // AI phrase generation temp storage
     const [enhancing, setEnhancing] = useState(false);
 
-    const [error, setError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("Error creating posts.");
-
+    useEffect(() => {
+        setValue("message", content)
+    }, [content]);
 
     const enhanceWithAI = async () => {
         setEnhancing(true);
@@ -46,43 +39,9 @@ export default function AddPost() {
         setEnhancing(false);
     };
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        // e.preventDefault();
-        //
-        // if (loading) return;
-        // setError(false);
-        //
-        // if (content.length < 1) {
-        //     setErrorMessage("Post cannot be empty");
-        //     setError(true);
-        // } else if (filter.isProfane(content)) {
-        //     setErrorMessage("Profanities are not allowed");
-        //     setError(true);
-        // } else {
-        //     try {
-        //         const filteredContent = filter.clean(content);
-        //
-        //         const res = await createPost(filteredContent);
-        //
-        //         if (res.ok) {
-        //             setContent("");
-        //             router.refresh();
-        //         }
-        //         setError(false);
-        //         setLoading(false);
-        //     } catch {
-        //         setErrorMessage("Error creating posts.");
-        //         setError(true);
-        //     }
-        // }
-    }
-
-
-    // Example profanity filter function
+    // Profanity filter function
     function containsProfanity(str: string) {
-        // Replace this with actual profanity checking logic
-        const profanities = ['badword1', 'badword2'];
-        return profanities.some((profanity) => str.includes(profanity));
+        return filter.isProfane(str)
     }
 
     // Zod Form Schema validation
@@ -97,19 +56,35 @@ export default function AddPost() {
             })
             .refine(value => !containsProfanity(value), {
                 message: "Message contains profanities",
-            }),
+            })
     })
 
     // Zod Form object
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
-        mode: `onChange`
+        mode: `onChange`,
+        defaultValues: {
+            message: ""
+        }
     })
 
+    const {
+        register,
+        control,
+        formState,
+        watch,
+        getValues,
+        setValue,
+        reset,
+        trigger,
+    } = form;
+
     async function onSubmit(data: z.infer<typeof FormSchema>) {
-        // console.log(JSON.stringify(data))
-        const res = await createPost(content);
+        const res = await createPost(data.message);
         if (res.ok) {
+            reset({
+                message: "",
+            })
             setContent("");
             router.refresh();
         }
@@ -141,13 +116,14 @@ export default function AddPost() {
                                         placeholder="Give it a try and test it out"
                                         className="resize-none h-28 rounded-lg"
                                         disabled={enhancing}
-                                        onChangeCapture={(e) => setContent(e.currentTarget.value)}
-                                        defaultValue={content}
+                                        onChangeCapture={(e) => {
+                                            setContent(e.currentTarget.value)
+                                        }}
                                         {...field}
                                     />
                                 </FormControl>
                                 <div id="form-error" className="mb-2 flex flex-row justify-between">
-                                    <div className="">
+                                    <div>
                                         <FormMessage className="text-rose-400 dark:text-rose-600"/>
                                     </div>
                                     <p className="text-xs pt-[2px]">{(content && content.length) || "0"}/300</p>
--- requests/deletePost.ts
+++ requests/deletePost.ts
@@ -1,5 +1,3 @@
-import process from "process";
-
 export async function deletePost(postId:string) {
     const [post] = await Promise.all([fetch(`/api/v1/post/${postId}`, {
         method: "DELETE",
