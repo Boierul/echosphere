2082dcd37fd352af94b3358d78a036f72680cf48
Boierul <49337867+Boierul@users.noreply.github.com>
2024-02-26 16:35:42 +0100

    Fix: Fix auth for Vercel deployment

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth/next";
 import {authOptions} from "@/utils/authOptions";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 // Export the handler for both GET and POST methods
-export const GET = handler.handlers.GET;
-export const POST = handler.handlers.POST;
+export { handler as GET, handler as POST};
