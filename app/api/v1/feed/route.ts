324a2568ac0d3161928fd60639521b6975628a16
Boierul <49337867+Boierul@users.noreply.github.com>
2024-02-28 03:38:03 +0100

    Finally did it :>

--- app/api/v1/feed/route.ts
+++ app/api/v1/feed/route.ts
@@ -1,8 +1,11 @@
 import {NextResponse} from "next/server";
 import {getAllPostsFromDB} from "@/prisma/CRUD";
+import {unstable_noStore as noStore} from 'next/cache';
 
 /* GET - get all posts from the DB */
 export async function GET() {
+    noStore();
+
     try {
         const data = await getAllPostsFromDB();
         console.log("\x1b[32m  [GET METHOD] - \x1b[33m api/v1/feed - \x1b[0m Fetch all posts");
--- requests/getAllPosts.ts
+++ requests/getAllPosts.ts
@@ -1,8 +1,6 @@
 import process from "process";
-import {unstable_noStore as noStore} from 'next/cache';
 
 export async function getAllPosts() {
-    noStore();
     const [posts] = await Promise.all([fetch(`${process.env.BASE_URL}/api/v1/feed`, { cache: 'no-cache' })]);
     return posts.json();
 }
