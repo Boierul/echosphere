9d1c09604aff2bebbea2b319db97242e22f096f3
Boierul <49337867+Boierul@users.noreply.github.com>
2024-02-28 03:36:10 +0100

    Finally did it :>

--- requests/getAllPosts.ts
+++ requests/getAllPosts.ts
@@ -1,6 +1,8 @@
 import process from "process";
+import {unstable_noStore as noStore} from 'next/cache';
 
 export async function getAllPosts() {
+    noStore();
     const [posts] = await Promise.all([fetch(`${process.env.BASE_URL}/api/v1/feed`, { cache: 'no-cache' })]);
     return posts.json();
 }
